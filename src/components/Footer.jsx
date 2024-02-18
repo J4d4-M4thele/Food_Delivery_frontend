14e11ccf06f9a23cbf64ba720178bc699c5ad609
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-02-18 15:59:20 +0200

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                <div className="banner">
+                    <div className="left">GRUB BUCKET</div>
+                    <div className="right">
+                        <p>55 Shanti Drive, Honeydew</p>
+                        <p>Open 07:00 - 20:00</p>
+                    </div>
+                </div>
+                <div className="banner">
+                    <div className="left">
+                        <p>Developed by Jada Mathele</p>
+                    </div>
+                    <div className="right">
+                        <p>All Rights Reserved by Jada Mathele</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
