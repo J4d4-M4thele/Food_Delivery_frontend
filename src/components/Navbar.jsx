ba8092752c23eb7521c7a96e6b72d9e1c778028c
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-02-18 11:57:12 +0200

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,35 @@
-import React, { useState } from 'react';
-import { Link } from 'react-scroll';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { data } from '../restApi.json';
-
+import React, { useState } from "react";
+import { data } from "../restApi.json";
+import { Link } from "react-scroll";
+import { GiHamburgerMenu } from "react-icons/gi";
 const Navbar = () => {
   const [show, setShow] = useState(false);
   return (
-    <nav>
-      <div className="logo">GRUB BUCK</div>
-      <div className={show ? "navLinks showmenu" : "navLinks"}>
-        <div className="links">
-          {
-            data[0].navbarLinks.map(element => {
-              return (
-                <Link
-                  to={element.link}
-                  key={element.id}
-                  spy={true}
-                  smooth={true}
-                  duration={500}
-                >
-                  {element.title}
-                </Link>
-              );
-            })}
+    <>
+      <nav>
+        <div className="logo">GRUB BUCKET</div>
+        <div className={show ? "navLinks showmenu" : "navLinks"}>
+          <div className="links">
+            {data[0].navbarLinks.map((element) => (
+              <Link
+                to={element.link}
+                spy={true}
+                smooth={true}
+                duration={500}
+                key={element.id}
+              >
+                {element.title}
+              </Link>
+            ))}
+          </div>
+          <button className="menuBtn">OUR MENU</button>
         </div>
-        <button className='menuBtn'>OUR MENU</button>
-      </div>
-      <div className="hamburger" onClick={() => setShow(show)}>
-        <GiHamburgerMenu />
-      </div>
-    </nav>
-  )
-}
+        <div className="hamburger" onClick={()=> setShow(!show)}>
+                <GiHamburgerMenu/>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
