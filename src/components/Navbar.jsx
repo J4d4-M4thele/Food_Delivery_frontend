67f1fae7297ebfe785e3cc912c7d53b99d791c07
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-02-18 11:53:08 +0200

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const [show, setShow] = useState(false);
   return (
     <nav>
-      <div className="logo">JADA</div>
+      <div className="logo">GRUB BUCK</div>
       <div className={show ? "navLinks showmenu" : "navLinks"}>
         <div className="links">
           {
